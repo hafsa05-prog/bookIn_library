78055a10922a2d401285b8949ddd0cd0b8e0277a
hafsa <hafxaaa14@gmail.com>
2026-04-28 06:15:47 +0500

    created login page UI  and dashboard layout

---
+++ app/src/main/java/com/books/bookapp/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.books.bookapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import android.widget.Button
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        // 1. Move the button logic INSIDE these brackets
+        val loginButton = findViewById<Button>(R.id.btnLogin)
+        loginButton.setOnClickListener {
+            val intent = Intent(this, dashboard::class.java)
+            startActivity(intent)
+        }
+
+        // 2. This part handles the screen edges
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
+
+//class MainActivity : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        // Find the button (make sure the ID matches what you set in the XML)
+//        val loginButton = findViewById<Button>(R.id.btnLogin)
+//
+//// Set the action for the click
+//        loginButton.setOnClickListener {
+//            val intent = Intent(this, dashboard::class.java)
+//            startActivity(intent)
+//        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//    }
+//}
